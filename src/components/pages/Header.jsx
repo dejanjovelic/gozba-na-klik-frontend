f092dd0a0e7c86c0f960877b52ba6d99f3392aeb
dejan.jovelic <dejan.jovelic@gmail.com>
2025-09-27 21:59:15 +0200

    feat: korekcija navbar elementa.

--- src/components/pages/Header.jsx
+++ src/components/pages/Header.jsx
@@ -1,5 +1,5 @@
 import { useAuth } from "../../config/AuthContext";
-import React from "react";
+import React, { useRef } from "react";
 import { useState, useEffect } from "react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import "../../styles/global.scss";
@@ -8,41 +8,45 @@ import { LogOut } from "lucide-react";
 const Header = () => {
   const location = useLocation();
   const navigate = useNavigate();
+  const dropdownRef = useRef(null);
+  const { isLoggedIn, logout } = useAuth();
+  const [open, setOpen] = useState(false);
 
   async function GetUserAsync() {
 
   }
 
-  const { isLoggedIn, logout } = useAuth();
-  const [open, setOpen] = useState(false);
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setOpen(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+
   useEffect(() => {
     setOpen(false);
   }, [isLoggedIn]); //Kada se promenmi isLoggedIn se zatvara dropdown, ovo radimo ako se izlogujemo i ulogujemo opet
   return (
     <nav>
-
-      <div className="button-section">
-        {!isLoggedIn ? (
-          <div>
-            <button onClick={() => { navigate("/login") }} id="logInBtn">Log In</button>
-            <button onClick={() => { navigate("/register") }} id="SignUpBtn">Sign Up</button>
-          </div>
-        ) : (
-        <>
-          <span
-            id="profileDropDownList"
-            onMouseEnter={() => setOpen(true)}
-            onMouseLeave={() => setOpen(false)}
-          >
-            <CircleUser size={40} />
+      {!isLoggedIn ? (
+        <div className="button-section">
+          <button onClick={() => { navigate("/login") }} id="logInBtn">Log In</button>
+          <button onClick={() => { navigate("/register") }} id="SignUpBtn">Sign Up</button>
+        </div>
+      ) : (
+        <div className="nav-bar-container" ref={dropdownRef}>
+          <span id="profileDropDownList" onClick={() => setOpen(prev => !prev)}>
+            <CircleUser size={35} />
           </span>
 
-          <div
-            id="DropDownMenuContent"
-            className={open ? "open" : ""}
-            onMouseEnter={() => setOpen(true)}
-            onMouseLeave={() => setOpen(false)}
-          >
+          <div id="DropDownMenuContent" className={open ? "open" : ""}>
             <div className="DropDownMenuItem">
               <Link to="/profile">
                 <CircleUser />
@@ -51,16 +55,17 @@ const Header = () => {
             </div>
             <div className="DropDownMenuItem">
               <button id="logoutButton" onClick={logout}>
-                <LogOut></LogOut>
+                <LogOut />
                 Logout
               </button>
             </div>
           </div>
-        </>
-        )}
-      </div>
+        </div>
+      )}
 
     </nav>
   );
+
 };
+
 export default Header;
