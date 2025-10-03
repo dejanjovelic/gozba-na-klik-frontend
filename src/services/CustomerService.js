907d41aa9fbf2933c1100a2a22f4e27fc66d4bcf
unknown <elhanhajrovic02@gmail.com>
2025-10-03 22:54:43 +0200

    feat: Implementirana stranica za adrese i CRUD nad adresama kupca

--- src/services/CustomerService.js
+++ src/services/CustomerService.js
@@ -1,9 +1,30 @@
 import React from "react";
 import AxiosConfig from "../config/AxiosConfig";
+import { Axios } from "axios";
 const RESOURCE = "/api/customers";
 
 export async function createCustomer(data) {
     const response = await AxiosConfig.post(`${RESOURCE}`, data,
         {headers:{"Content-Type":"application/json"}})
+    return response.data;
+}
+
+export async function getAddresses(customerId) {
+    const response = await AxiosConfig.get(`${RESOURCE}/${customerId}/addresses`);
+    return response.data;
+}
+
+export async function createAddress(customerId, data) {
+    const response = await AxiosConfig.post(`${RESOURCE}/${customerId}/addresses`, data);
+    return response.data;
+}
+
+export async function updateAddress(customerId, addressId, data) {
+    const response = await AxiosConfig.put(`${RESOURCE}/${customerId}/addresses/${addressId}`, data);
+    return response.data;
+}
+
+export async function deleteAddress(customerId, addressId) {
+    const response = await AxiosConfig.delete(`${RESOURCE}/${customerId}/addresses/${addressId}`);
     return response.data;
 }
